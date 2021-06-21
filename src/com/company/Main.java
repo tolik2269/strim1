6b2777c1018d8a73e8230423f55dd7d4ca89035e
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2021-06-21 23:02:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        for (int e: intList){
+            if (e>0 && e%2==0){
+
+                System.out.println(e);
+            }
+
+               }
+
+
+    }
+}
---
+++ src/com/company/StreamMain.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> stream = intList.stream()
+                .filter(i -> i>0)
+                .filter(i -> i%2==0)
+                .sorted(Comparator.naturalOrder());
+
+        System.out.println(stream);
+
+    }
+}
